5d72e7def43a3ee955c1d8ace5ff4ac4a5d59f56
Niko Wessman <niko.wessman@nsd.fi>
2016-11-19 19:59:59 +0200

    Initial commit

---
+++ NsdWebApiUnitTestingTraining/UnitTests.cs
@@ -0,0 +1,33 @@
+﻿// First Nuget: Newton, RestSharp
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace NsdWebApiUnitTestingTraining {
+    [TestClass]
+    public class UnitTests {
+        RestClient client = new RestClient("https://jsonplaceholder.typicode.com"); // Sample REST
+        //RestClient client = new RestClient("http://192.168.1.94:8088");
+
+        [TestMethod]
+        public void TestGet() {
+            var request = new RestRequest("posts/1", Method.GET);
+            JObject obj = JObject.Parse(client.Execute(request).Content);
+            string result = obj["userId"].ToString();
+            Assert.AreEqual("1", result);
+        }
+
+        [TestMethod]
+        public void TestPost() {
+            var request = new RestRequest("posts", Method.POST);
+            var data = new { title = "foo", body = "bar", userId = 1 };
+            request.AddJsonBody(data);
+            JObject obj = JObject.Parse(client.Execute(request).Content);
+            string result = obj["id"].ToString();
+            Assert.AreEqual("101", result);
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+#ASP.Net WebAPI unit testing through REST calls
+
+Contribute: www.nsd.fi
